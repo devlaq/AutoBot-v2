5630c4bb105b2e49b760caca99d9c15febf12d23
Black <worldisblack.1@gmail.com>
2021-03-29 09:47:08 +0900

    Module 'typescript' added

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -9,7 +9,15 @@ import { strikethrough } from "chalk";
 class CommandSendEvent implements Executor {
     public tag = 'help.ts';
     execute = (client: Client, message: Discord.Message, args: string[]): void => {
-        const embed = Utils.Embed.createEmbed('도움말', message.author);
+        const embed = Utils.Embed.createEmbed(message.author);
+        if(args.length > 0) {
+            if(client.commands.has(args[0])) {
+                   embed.setTitle(`도움말 - ${args[0]}`);
+                
+            } else {
+                embed.setTitle(`도움말`);
+            }
+        }
         client.commands.forEach((v, k) => {
             embed.addField(`${v.commandName} - ${v.hideDescription ? '__Description hided__': v.description}${v.indev ? ' - __InDev__' : ''}`, `사용법: ${v.hideUsage ?  '__Usage hided__' : v.usage[0]}\n자세한 사용법은 /help ${v.commandName}을(를) 참조하세요.`);
         })
